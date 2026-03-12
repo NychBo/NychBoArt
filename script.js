6123fd62865427ec687d76458d75239a74d636f1
NychBo <dark2dreamer2@gmail.com>
2026-03-12 16:17:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function setLang(lang){
 
 document.querySelectorAll("[data-ua]").forEach(el=>{
-
 el.innerText=el.dataset[lang]
-
 })
 
 }
 
+
+
+let images=[]
+let index=0
+
 function openImage(img){
 
-document.getElementById("viewer").style.display="flex"
+images=[...document.querySelectorAll(".gallery img")]
 
+index=images.indexOf(img)
+
+document.getElementById("viewer").style.display="flex"
 document.getElementById("viewer-img").src=img.src
 
 }
@@ -21,3 +27,21 @@ function closeImage(){
 document.getElementById("viewer").style.display="none"
 
 }
+
+document.addEventListener("keydown",function(e){
+
+if(e.key==="ArrowRight"){
+
+index=(index+1)%images.length
+document.getElementById("viewer-img").src=images[index].src
+
+}
+
+if(e.key==="ArrowLeft"){
+
+index=(index-1+images.length)%images.length
+document.getElementById("viewer-img").src=images[index].src
+
+}
+
+})
